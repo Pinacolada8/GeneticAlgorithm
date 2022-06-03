9930cf2c6b581e7c73b06304f2a2d9cdc49ea344
MATHEUS DUTRA CERBINO <mdcmatheusmdc@gmail.com>
2022-06-03 10:27:16 -0300

    More Code

--- GeneticAlgorithm/GeneticAlgorithm.cs
+++ GeneticAlgorithm/GeneticAlgorithm.cs
@@ -10,11 +10,13 @@ public class IndividualInformation
     public double Value { get; set; }
 
     public double Fitness { get; set; }
+
+    public double FitnessAccumulated { get; set; }
 }
 
 public class GeneticAlgorithm
 {
-    public void RunOnce(List<Individual> individuals, double idealSolution, double errorRange)
+    public IndividualInformation RunOnce(List<Individual> individuals, double idealSolution, double errorRange)
     {
         var inds = individuals.Select(x => new IndividualInformation()
         {
@@ -24,6 +26,18 @@ public class GeneticAlgorithm
 
         inds = CalcFitness(inds).ToList();
 
+        var best = inds.First();
+
+        if(Math.Abs(best.Value - idealSolution) < errorRange)
+            return best;
+
+        var selectedIndividuals = SelectIndividuals(inds, individuals.Count);
+
+        var childs = CrossOver(selectedIndividuals);
+
+
+
+        return best;
     }
 
     public double CalcIndividual(Individual individual)
@@ -46,5 +60,56 @@ public class GeneticAlgorithm
         var result = individuals.OrderBy(x => x.Fitness);
 
         return result;
+    }
+
+    public List<IndividualInformation> SelectIndividuals(List<IndividualInformation> individuals, int qtd)
+    {
+        var rnd = new Random();
+        var sum = 0.0;
+        foreach(var individual in individuals)
+        {
+            individual.FitnessAccumulated = sum;
+            sum += individual.Fitness;
+        }
+
+        var selectedIndividuals = new List<IndividualInformation>();
+        for(var i = 0; i < qtd; i++)
+        {
+            var rndValue = rnd.NextDouble() * sum;
+
+            var selected = individuals.First(x => x.FitnessAccumulated >= rndValue);
+
+            selectedIndividuals.Add(selected);
+        }
+
+        return selectedIndividuals;
+    }
+
+    public List<Individual> CrossOver(List<IndividualInformation> individuals)
+    {
+        var childs = new List<Individual>();
+        var halfSize = individuals.Count / 2;
+        for(var i = 0; i < halfSize; i++)
+        {
+            var parent1 = individuals[i];
+            var parent2 = individuals[i + halfSize];
+
+            var rnd = new Random();
+            var alfa = rnd.NextDouble();
+            childs.AddRange(CrossOver(parent1.Individual, parent2.Individual, alfa));
+
+        }
+
+        return childs;
+    }
+
+    public List<Individual> CrossOver(Individual parent1, Individual parent2, double alfa)
+    {
+
+        var x1Arr = parent1.X.Select((t, i) => alfa * t + (1 - alfa) * parent2.X[i]).ToList();
+
+        var x2Arr = parent1.X.Select((t, i) => alfa * parent2.X[i] + (1 - alfa) * t).ToList();
+
+        return new() { new() { X = x1Arr }, new() { X = x2Arr } };
     }
 }
